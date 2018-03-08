a4e63f387eb98e9e9ad413992c6add51ea0809ea
Mishal Shah <shahmishal1998@gmail.com>
2018-03-08 22:54:42 +0530

    Index updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Note = require('../models/notes');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/notes_show/:id', function(req, res, next) {
+	var id = req.params.id;
+    Note.findOne({_id: id},function(err,notes){
+        if(err)
+            res.send(err);
+        else
+        {
+            res.render('index',{title:notes.title , content: notes.content, imageURL: notes.imageURL});
+        }
+    })
 });
 
 module.exports = router;
